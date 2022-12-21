7d1d6bd64b7e919c2e71c5bd790c0a885e6cbe94
AndriiSmolinskyi <awful.braid@gmail.com>
2022-12-21 02:01:17 +0200

    Lab1
    Labor1

---
+++ Labor1/Labor1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+//стандартні бібліотеки
+using namespace std;
+//використання простору стд, щоб не вводити весь час в тілі
+
+int main()
+{
+    cout << "How many years did the Ukrainian revolution last (1917-1921)?\n";
+    //Вивід в консоль
+
+    int sum;
+    //для перевірки відповіді
+    cin >> sum;
+
+    if (sum == 4)   //якщо відповідь правильна
+    {
+        cout << "Nice, you good in history\n";
+    }
+
+    else //якщо відповідь неправильна
+    {
+        cout << "Bad answer\n";
+    }
+
+}
